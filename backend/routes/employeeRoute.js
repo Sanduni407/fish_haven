ab62da507820f1df5057eba6b4ed720a7a93302c
Avishka Vikum Hettige <158188972+Avishka137@users.noreply.github.com>
2025-03-20 18:59:47 +0530

    update leave request and the related functions

--- backend/routes/employeeRoute.js
+++ backend/routes/employeeRoute.js
@@ -1,11 +1,16 @@
 import express from 'express'
 import userAuth from '../middleware/auth.js';
-import { CreateAleave, getRequestsByUserId } from '../controllers/leaveController.js';
+import { CreateAleave, getRequestsByUserId,updateRequest,getRequestByRequestId,DeleteRequest,getAllRequests,updateLeavestatus } from '../controllers/leaveController.js';
 
 
 const employeeRouter = express.Router();
 
 employeeRouter.post('/create-leave',userAuth,CreateAleave);
 employeeRouter.post('/get-requests-byuser',userAuth, getRequestsByUserId);
+employeeRouter.put('/update-request/:id',updateRequest);
+employeeRouter.post('/fetch-a-request',getRequestByRequestId);
+employeeRouter.delete('/delete-a-request/:id',DeleteRequest);
+employeeRouter.get('/get-all-requests',getAllRequests);
+employeeRouter.put('/update-status/:id',updateLeavestatus);
 
 export default employeeRouter; 
