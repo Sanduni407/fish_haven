35821b35a46daf4ec1c240463460421c8c877f1e
minuli2002 <minulidissanayake228@gmail.com>
2025-03-20 13:55:34 +0530

    implement create delivery part

---
+++ backend/models/deliverModel.js
@@ -0,0 +1,19 @@
+import mongoose from "mongoose"
+
+const DeliverySchema = new mongoose.Schema({
+    
+    userId:{ type:String, required:true}, // fetch karaddima ganna
+    orderCode:{type:String,required:true},
+    shippingAddress:{type:String, required:true},
+    deliveryDate:{type:String, required:true},
+    orderType:{type:String, required:true},
+    contact:{type:String, required:true },
+    PackagingArray:{type:Array,default:'send later'},
+    delCode:{type:String,required:true},
+    Deliverystatus:{type:String,default:'Pending'}
+
+})
+
+const DeliveryModel = mongoose.model.delivery || mongoose.model("delivery",DeliverySchema);
+
+export default DeliveryModel;
