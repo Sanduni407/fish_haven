12f093a6bb0c786a352ebfa7d7557daee0a3e853
SanjithaRa <sanjithar298@gmail.com>
2025-03-20 18:37:56 +0530

    complete  fish controller and fish route

--- backend/controllers/fishController.js
+++ backend/controllers/fishController.js
@@ -48,4 +48,76 @@ const getallFish= async(req,res)=>{
     }
 }
 
-export {addfish,getfishById,getallFish}
+
+const getUniqueFishNames = async (req, res) => {
+    try {
+        const uniqueFishNames = await fishModel.distinct("fishCategory");
+        res.json({ success: true, uniqueFishNames });
+    } catch (error) {
+        console.error(error);
+        res.json({ success: false, message: "Error fetching fish names" });
+    }
+};
+
+const updateFish = async(req,res)=>{
+
+    const {id} = req.params; // extract unique fish id
+    const {fishCategory, gender, size, unitPrice,quantity } = req.body;
+
+    try{
+          const updatefish = await fishModel.findByIdAndUpdate(id, {
+            fishCategory, gender, size, unitPrice,quantity }, { new: true });
+            
+      if (!updatefish) return res.status(404).json({ success: false, message: 'Fish not found' });
+
+      res.json({ success: true, message: 'Fish updated successfully' });
+
+    }catch(err)
+    {
+        console.log(err)
+
+        res.json({ success: false, message: err.message });
+    }
+  
+
+}
+
+
+const getfishByFishId = async(req,res)=>{ // get fish by user id
+
+    try{
+        
+            const {id} = req.params
+            const fish = await fishModel.findOne({_id:id});
+
+            res.json({success:true,fish});
+
+    }catch(err)
+    {
+        console.log(err)
+        res.json({success:false,message:'Error'})
+    }
+}
+
+const deleteFish= async(req,res)=>{
+    try
+    {
+       const{id} = req.params;
+ 
+      if(!id)
+      {
+       return res.json({success:false , message:'required data is missing'})
+      }
+ 
+          await fishModel.findByIdAndDelete(id);
+ 
+        res.json({success:true,message:'deleted'})
+ 
+    }catch(err){
+ 
+       console.log(err);
+       res.json({success:false,message:'Error'})
+    }
+ }
+
+export {addfish,getfishById,getallFish,getUniqueFishNames,updateFish,deleteFish,getfishByFishId}
--- backend/routes/fishRoute.js
+++ backend/routes/fishRoute.js
@@ -1,12 +1,16 @@
 
 import express from 'express'
 import userAuth from '../middleware/auth.js';
-import { addfish, getfishById, getallFish} from '../controllers/fishController.js';
+import { addfish, getfishById, getallFish,getUniqueFishNames,deleteFish,updateFish,getfishByFishId} from '../controllers/fishController.js';
 
 const fishRouter = express.Router();
 
 fishRouter.post('/create-fish', userAuth,addfish)
 fishRouter.post('/getfishById',userAuth,getfishById)
 fishRouter.get('/getallFish',getallFish)
+fishRouter.get('/get-fish-names',getUniqueFishNames)
+fishRouter.delete('/delete-fish/:id',deleteFish)
+fishRouter.put('/update-fish/:id',updateFish)
+fishRouter.get('/get-fish-id/:id',getfishByFishId)
 
 export default fishRouter;
