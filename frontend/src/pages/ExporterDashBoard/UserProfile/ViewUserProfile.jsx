6c67ecda648a41d0a6471f570b886f1fa7e10201
Danu20019 <it23206564@my.sliit.lk>
2025-03-21 12:13:29 +0530

    edit user profiles

---
+++ frontend/src/pages/ExporterDashBoard/UserProfile/ViewUserProfile.jsx
@@ -0,0 +1,178 @@
+import React, { useContext, useEffect, useState } from 'react'
+import './ViewUserProfile.css'
+import Card from 'react-bootstrap/Card';
+import ListGroup from 'react-bootstrap/ListGroup';
+import SideNavBar from '../../../components/SideNavBar/SideNavBar';
+import { AppContext } from '../../../context/AppContext';
+import axios from 'axios';
+import Button from 'react-bootstrap/Button';
+import Modal from 'react-bootstrap/Modal';
+import Col from 'react-bootstrap/Col';
+import Form from 'react-bootstrap/Form';
+import Row from 'react-bootstrap/Row';
+
+const ViewUserProfile = () => {
+
+    const {token} = useContext(AppContext)
+
+    const[id,setId] = useState('')
+    const[name,setName] = useState('')
+    const[ businessName,setbusinessName] = useState('')
+    const[businessRegNo,setbusinessRegNo] = useState('')
+    const[  email,setemail] = useState('')
+    const[ phone,setphone] = useState('')
+    const[ address,setaddress] = useState('')
+
+    const [modalShow, setModalShow] = useState(false);
+
+
+    const fetchUserProfileData = async()=>{
+     
+        try{
+    
+          const response = await axios.post('http://localhost:4000/api/auth/get-user',{},{headers:{token}});
+    
+          if(response.data.success)
+          {
+            console.log(response.data.user)
+           
+            const user = response.data.user;
+            setId(user._id)
+            setName(user.name)
+            setbusinessName(user.businessName)
+            setbusinessRegNo(user.businessRegNo)
+            setemail(user.email)
+            setphone(user.phone)
+            setaddress(user.address)
+          }
+          else
+          {
+            console.log('Error')
+          }
+    
+        }catch(err)
+        {
+          console.log(err);
+        }
+      }
+
+      useEffect(()=>{
+        fetchUserProfileData()
+      },[])
+
+
+      const updateUser = async()=>{
+     
+        try{
+    
+          const response = await axios.put(`http://localhost:4000/api/auth/update-user/${id}`,
+            {name, businessName, businessRegNo, address, email, phone});
+    
+          if(response.data.success)
+          {
+            console.log('upated successfully')
+            fetchUserProfileData()
+            setModalShow(false)
+          }
+          else
+          {
+            console.log('Error')
+          }
+    
+        }catch(err)
+        {
+          console.log(err);
+        }
+      }
+
+
+  return (
+    <div>
+
+   <div className="view-user-profile-container">
+      <div className="left-column">
+       <SideNavBar role={'Exporter'} />
+      </div>
+   <div className="right-column">
+
+   <Card style={{ width: '40rem' ,marginTop:'50px' , backgroundColor: '#f8f9fa'}} >
+      <Card.Body>
+        <Card.Title>My Profile</Card.Title>
+        <Card.Text>
+          
+        </Card.Text>
+      </Card.Body >
+      <ListGroup className="list-group-flush" >
+        <ListGroup.Item style={{ backgroundColor: '#f8f9fa'}}><span style={{color:'#49557e', fontWeight:'bold',marginRight:'10px'}}>Full Name :</span> {name}</ListGroup.Item>
+        <ListGroup.Item style={{ backgroundColor: '#f8f9fa'}}> <span style={{color:'#49557e', fontWeight:'bold',marginRight:'10px'}}>Email : </span>{email} </ListGroup.Item>
+        <ListGroup.Item style={{ backgroundColor: '#f8f9fa'}}><span style={{color:'#49557e', fontWeight:'bold',marginRight:'10px'}}>Business Name :</span> {businessName} </ListGroup.Item>
+        <ListGroup.Item style={{ backgroundColor: '#f8f9fa'}}><span style={{color:'#49557e', fontWeight:'bold',marginRight:'10px'}}>Business RegNo :</span> {businessRegNo}</ListGroup.Item>
+        <ListGroup.Item style={{ backgroundColor: '#f8f9fa'}}><span style={{color:'#49557e', fontWeight:'bold',marginRight:'10px'}}>Contact No :</span> {phone}</ListGroup.Item>
+        <ListGroup.Item style={{ backgroundColor: '#f8f9fa'}}><span style={{color:'#49557e', fontWeight:'bold',marginRight:'10px'}}>Address :</span> {address} </ListGroup.Item>    
+      </ListGroup>
+      <Card.Body>
+      <Button  style={{width:'130px', backgroundColor:'#49557e', color:'white',border:'none'}} onClick={()=>{setModalShow(true)}}>Edit Profile</Button>
+      </Card.Body>
+    </Card>
+
+   </div>
+   </div>
+
+
+   <Modal show={modalShow} onHide={()=>{setModalShow(false)}} centered>
+          <Modal.Header closeButton>
+            <Modal.Title style={{color:'#49557e'}} onClick={ fetchUserProfileData}>Edit Profile Details</Modal.Title>
+          </Modal.Header>
+          <Modal.Body>
+          <Form style={{marginTop:'30px'}}>
+        <Row>
+        <Col>
+            <Form.Control type='text' value={name} onChange={(e)=>{setName(e.target.value)}} />
+          </Col>
+          </Row>
+
+          <Row>
+  
+          <Col>
+           
+          <Form.Control value={email} onChange={(e)=>{setemail(e.target.value)}} />
+          </Col>
+          </Row>
+
+          <Row>
+          <Col>
+          <Form.Control value={businessName} onChange={(e)=>{setbusinessName(e.target.value)}}/>
+            
+          </Col>
+          
+          <Col>
+            <Form.Control value={businessRegNo} onChange={(e)=>{setbusinessRegNo(e.target.value)}}   />
+          </Col>
+          </Row>
+          <Row>
+        <Col>
+            <Form.Control type='Number' value={phone} onChange={(e)=>{setphone(e.target.value)}}  />
+          </Col>  
+          </Row>
+          <Row>
+          <Col>
+            <Form.Control type='text' value={address} onChange={(e)=>{setaddress(e.target.value)}}   />
+          </Col>  
+        </Row>
+      </Form>  
+          </Modal.Body>
+          <Modal.Footer>
+          <Button variant="secondary" style={{backgroundColor:'#49557e', color:'white', width:"200px"}} onClick={updateUser}>
+              Update
+            </Button>
+          </Modal.Footer>
+        </Modal>
+  
+      
+    </div>
+
+    
+  )
+}
+
+export default ViewUserProfile
