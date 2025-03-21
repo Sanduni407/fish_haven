3f8c7ad64ba754d6f0afc11106c4214b7665282e
Danu20019 <it23206564@my.sliit.lk>
2025-03-21 11:06:10 +0530

    login frontend part

---
+++ frontend/src/pages/Login/Login.jsx
@@ -0,0 +1,84 @@
+import React, { useContext, useState } from 'react'
+import './Login.css'
+import { useNavigate } from 'react-router-dom';
+import { AppContext } from '../../context/AppContext';
+import axios from 'axios'
+import { toast } from 'react-toastify';
+
+
+
+const Login = () => {
+
+  const navigate =useNavigate()
+
+  const {token , setToken} = useContext(AppContext)
+ 
+  const [email,setEmail] = useState("");
+  const [password,setPassword] = useState("");
+
+  const onSubmitHandler = async(e)=>{
+    try{
+       e.preventDefault();
+
+       console.log("Submit button clicked");
+        
+       const response = await axios.post('http://localhost:4000/api/auth/login', {email,password});
+
+       if(response.data.success)
+       {
+        console.log("successfully logged")
+        console.log(response.data)
+
+        setToken(response.data.token)
+    
+        localStorage.setItem("token",response.data.token)
+
+        const role = await response.data.role;
+
+          if(role === 'Exporter')
+          {
+               navigate('/exporter-dashboard');
+          }
+          else if(role === 'Admin')
+          {
+              navigate('/admin-dashboard');
+          }
+          else if(role === 'Supplier')
+            {
+                navigate('/supplier/add-fish');
+            }
+            else if(role === 'Employer')
+              {
+                  navigate('/employee/dashboard');
+              }
+         
+          
+       }
+    }catch(error){
+
+      console.log(error)
+     
+
+    }
+  }
+
+
+  return (
+    <div className="login-container">
+    <div className="login-box">
+      <h2>Sign In</h2>
+      <form onSubmit={onSubmitHandler}>
+        <input onChange={e => setEmail(e.target.value)} value={email} type="email" placeholder="Email" required />
+
+        <input onChange={e => setPassword(e.target.value)} value={password} type="password" placeholder="Password" required />
+
+        <a className="forgot-password" onClick={()=>navigate('/password-reset')}>Forgot Password?</a>
+
+        <button type="submit">Sign In</button>
+      </form>
+    </div>
+  </div>
+  )
+}
+
+export default Login
