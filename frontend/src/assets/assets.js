8d74eeac7270720387a015c9364e78316f04c8bd
SanjithaRa <sanjithar298@gmail.com>
2025-05-02 18:15:06 +0530

    set up the image adding

---
+++ frontend/src/assets/assets.js
@@ -0,0 +1,13 @@
+import upload_area from './upload_area.jpg'
+import download from './downloads.jpg'
+import trash from './delete.jpg'
+import view from './vie.jpg'
+
+
+export const assets ={
+   
+    upload_area,
+    download,
+    trash,
+    view
+}
