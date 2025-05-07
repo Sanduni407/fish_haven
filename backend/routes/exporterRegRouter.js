71f67e2a2f67b55344c5fb7837e78012c571b393
Sanduni407 <sandunidias05@gmail.com>
2025-05-07 14:23:30 +0530

    frontend dahsboards

--- backend/routes/exporterRegRouter.js
+++ backend/routes/exporterRegRouter.js
@@ -1,5 +1,5 @@
 import express from 'express'
-import { createRegisterRequest, getallRequest, getARequestById } from '../controllers/exporterRegController.js';
+import { createRegisterRequest, deleteRegisterRequest, getallRequest, getARequestById } from '../controllers/exporterRegController.js';
 
 
 const requestRouter = express.Router();
@@ -7,6 +7,7 @@ const requestRouter = express.Router();
 requestRouter.post('/create-request', createRegisterRequest);
 requestRouter.get('/get-all-requests', getallRequest);
 requestRouter.get('/get-a-request/:id', getARequestById);
+requestRouter.delete('/delete-a-request/:id', deleteRegisterRequest);
 
 
 export default requestRouter; 
