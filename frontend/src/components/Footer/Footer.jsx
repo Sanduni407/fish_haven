80311a1059ee0debc4191202f081a9a024e7f206
Danu20019 <it23206564@my.sliit.lk>
2025-05-08 12:47:49 +0530

    logo edit

--- frontend/src/components/Footer/Footer.jsx
+++ frontend/src/components/Footer/Footer.jsx
@@ -1,18 +1,21 @@
-import React from 'react'
-import './Footer.css'
-import { assets } from '../../assets/assets'
+import React from 'react';
+import './Footer.css';
+import { assets } from '../../assets/assets';
 
 const Footer = () => {
   return (
     <div className='footer' id='footer'>
         <div className="footer-content">
             <div className="footer-content-left">
-                <img  src={assets.logo}/>
+                <div className="footer-logo-container">
+                    <img src={assets.logo} alt="Fish-Haven Logo" className='footer-logo-img' />
+                    <span className='footer-logo-text'>Fish-Haven</span>
+                </div>
                 <p>Our fish are vibrant, healthy, and carefully bred to bring color and life to any aquarium</p>
                 <div className="footer-social-icons">
-                    <img src={assets.facebook_icon} alt="" />
-                    <img src={assets.twitter_icon} alt="" />
-                    <img src={assets.linkedin_icon} alt="" />
+                    <img src={assets.facebook_icon} alt="Facebook" />
+                    <img src={assets.twitter_icon} alt="Twitter" />
+                    <img src={assets.linkedin_icon} alt="LinkedIn" />
                 </div>
             </div>
             <div className="footer-content-right">
@@ -21,7 +24,6 @@ const Footer = () => {
                     <li>Home</li>
                     <li>About us</li>
                     <li>Fish Collection</li>
-                    
                 </ul>
             </div>
             <div className="footer-content-center">
@@ -35,7 +37,7 @@ const Footer = () => {
         <hr/>
         <p className="footer-copyright">Copyright 2024 fishhaven.com - All Right Reserved</p>
     </div>
-  )
-}
+  );
+};
 
-export default Footer
+export default Footer;
